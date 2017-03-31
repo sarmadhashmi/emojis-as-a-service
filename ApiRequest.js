bd227f25f7331337a544a66f1ede00d9fc0daea1
BenEmdon <benjaminemdon@gmail.com>
2017-03-31 14:27:21 -0400

    added request options

--- ApiRequest.js
+++ ApiRequest.js
@@ -3,7 +3,18 @@ const rp = require('request-promise');
 const router = express.Router();
 
 router.post('/', (req, res) => {
-  res.send({'cool': 'stuff'});
+  const opetions = {
+    method: 'POST',
+    uri: 'http://api.kairos.com/',
+    headers: {
+      app_id: process.env.API_ID,
+      app_key: process.env.API_KEY,
+    },
+    body: {
+      image: req.body.image,
+    },
+    json: true,
+  }
 });
 
 module.exports = router;
