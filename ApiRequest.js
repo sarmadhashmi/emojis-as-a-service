30ea2ed8ef64ab11c7671339d54f04b6158b6d86
BenEmdon <benjaminemdon@gmail.com>
2017-03-31 14:09:11 -0400

    hooked up a res

--- ApiRequest.js
+++ ApiRequest.js
@@ -2,4 +2,8 @@ const express = require('express');
 const rp = require('request-promise');
 const router = express.Router();
 
+router.post('/', (req, res) => {
+  res.send({'cool': 'stuff'});
+});
+
 module.exports = router;
